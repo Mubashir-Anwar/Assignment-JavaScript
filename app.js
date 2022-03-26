39bdfe429c2c413d8bc0cbedbf7a3232e8fa9bdf
muhammadmubashir.mmm@gmail.com <muhammadmubashir.mmm@gmail.com>
2022-03-26 12:16:12 +0500

    Done

---
+++ app.js
@@ -0,0 +1,164 @@
+// --------------Chapter 1 (ALERT)--------------
+// Question 1
+// alert("welcome to the website");
+
+// Question 2
+// alert("Error! Please enter a valid password")
+
+// Question 3
+// alert('Welcome to the JS Land...\n Happy coding!');
+
+//Question 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!");
+
+//Question 5
+// alert("Hello... I can write JS through my web browser's console");
+
+//-----------------Chapter 2 (Variable)-------------------------
+//Question 1
+// var userName= "My Name"
+// alert (username);
+
+//Question 2
+// var myName= "Mubashir Anwar"
+// alert(myName);
+
+//Question 3
+// var message= "Hello World"
+// alert(message);
+
+// Question 4
+// var userName="Mubashir Anwar"
+// var age= 24
+// var student="Web & Mobile Development"
+// alert(userName);
+// alert(age);
+// alert(student);
+
+//Question 5
+// var message="PIZZA \n PIZZ \n PIZ \n PI \n p"
+// alert(message);
+
+//Question 6
+// var emailAddress="muhammadmubashir.mmm@gmail.com"
+// alert("My Email address is " + emailAddress);
+
+//Question 7
+// var book="A smart way to learn JavaScript"
+// alert("I am trying to learn from the Book " + book);
+
+//Question 8
+// var write= "Yah! I can write HTML content through JavaScript"
+// document.write(write);
+
+//Question 9
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(design);
+
+//-------------------------Chapter 3 (VARIABLES FOR NUMBERS)-------------------------
+//Question 1
+// var age= 24
+// alert("I am " + age + " years old")
+
+//Question 2
+// var number= 10
+// alert("You have visited this site " + number + " times");
+
+//Question 3
+// var birthYear= 1997
+// document.write("My birth year is " + birthYear);
+
+//Question 4
+// var a="Mubashir"
+// var b=" T-Shirt"
+// var c=4
+// document.write(a +" ordered " + c + b);
+
+// ------------------Chapter 4 (VARIABLE NAMES: LEGAL & ILLEGAL)----------------
+//Question 1
+// var a, b, c;
+// a=15 , b=30 , c=10;
+// alert(a+b+c);
+
+//Question 2
+//legal variable are:  MyName, my_name , myname , $myname, _myname
+// Illegal variable are: my name , MYNAME, my-name , my.name , 1stname
+
+//Question 3
+// var x= document.createElement("H1");
+// var t=document.createTextNode("Welcome to My Homepage");
+// x.appendChild(t);
+//  document.body.appendChild(x);
+//  var a=", numbers, $ and _"
+//  var b=" letter, $ or _"
+//  var c="case sensitive"
+//  var d="keyword"
+// document.write ("Variable names can only contain" + a );
+// document.write ("Variables must begin with a" + b);
+// document.write ("Variable names are case " + c);
+// document.write ("Variable names should not be JS " + d);
+
+// Question 4
+
+//-----------------Chapter 5 (MATH EXPRESSIONS)------------------------------------
+//Question 1
+// var a=5 , b= 8 , c=10;
+// var x=(a+b+c);
+// document.write(x);
+
+//Question 2
+// var a=5 , b= 8 , c=10;
+// var x=(a-b-c);
+// document.write(x);
+
+// var a=5 , b= 8 , c=10;
+// var x=(a*b/c);
+// document.write(x);
+
+//Question 3
+// var a= 5
+// // document.write("Initial value: " + a)
+// // document.write("value after increment: "+ ++a)
+// var b= 7
+// x= ++a + b
+// // document.write ("Value after increment: " + x);
+// y= --x
+// // document.write("Value after decrement: " + y)
+// // p= y/12
+// // document.write("The remainder is: " + p)
+
+//Question 4
+// var ticketPrice=600
+//  x="Total cost to buy "
+//  p= 5
+//  y= ticketPrice * p
+//  z=" tickets to a movie is "
+// document.write(x + p + z + y);
+
+//Question 5
+// var number=4
+// var a=1 , b=2 , c=3 , d=4, e=5, f=6, g=7, h=8, i=9, j=10;
+// document.write(number * a)
+// document.write(number * b)
+// document.write(number * c)
+// document.write(number * d)
+// document.write(number * e)
+// document.write(number * g)
+// document.write(number * h)
+// document.write(number * i)
+// document.write(number * j)
+
+//Question 6
+
+// Question 7
+// var a=650;
+// var x=3;
+// var g=a*x;
+// var b=100;
+// var y=7;
+// var h=b*y   
+// var c=100;
+// document.write( g + h + c)
+
+// Question 8
